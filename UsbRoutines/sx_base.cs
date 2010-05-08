cd6c199c6ccaeda8ae49fc4b3d5eb2be0210c884
Bret McKee <bretm@boneheads.us>
2010-05-08 00:49:01 +0000

    Some installer related changes

--- UsbRoutines/sx_base.cs
+++ UsbRoutines/sx_base.cs
@@ -71,12 +71,12 @@ namespace sx
         internal const Byte SX_CMD_LOAD_EEPROM = 15;
 
         // flags
-        internal const Byte SX_CCD_FLAGS_FIELD_ODD = 1;       // Specify odd field for MX cameras
+        internal const Byte SX_CCD_FLAGS_FIELD_ODD = 1;        // Specify odd field for MX cameras
         internal const Byte SX_CCD_FLAGS_FIELD_EVEN = 2;	   // Specify even field for MX cameras
         internal const Byte SX_CCD_FLAGS_NOBIN_ACCUM = 4;	   // Don't accumulate charge if binning
         internal const Byte SX_CCD_FLAGS_NOWIPE_FRAME = 8;	   // Don't apply WIPE when clearing frame
         internal const Byte SX_CCD_FLAGS_TDI = 32;	           // Implement TDI (drift scan) operation
-        internal const Byte SX_CCD_FLAGS_NOCLEAR_FRAME = 64;  // Don't clear frame, even when asked
+        internal const Byte SX_CCD_FLAGS_NOCLEAR_FRAME = 64;   // Don't clear frame, even when asked
 
         // STAR2K values
         internal const UInt16 SX_STAR2K_STOP  = 0;
