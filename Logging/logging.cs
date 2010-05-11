7ca03853ee54e43834ad9640931c1de221a645b3
Bret McKee <bretm@boneheads.us>
2010-05-11 03:55:15 +0000

    Moved logging to its own class

---
+++ Logging/logging.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Logging
+{
+    public class Log
+    {
+        private const string logPath = @"c:\temp\sx_log.txt";
+        private static FileStream logFS = null;
+        private static DateTime lastWriteTime;
+
+        static Log()
+        {
+            try
+            {
+                logFS = new FileStream(logPath, FileMode.Create, FileAccess.Write, FileShare.Read, 1);
+                lastWriteTime = DateTime.Now;
+            }
+            catch
+            {
+            }
+
+            //logFS = File.Create(logPath);
+        }
+
+        public static void Write(string value)
+        {
+            if (logFS != null)
+            {
+                lock (logPath)
+                {
+                    DateTime currentTime = DateTime.Now;
+                    TimeSpan delta = currentTime - lastWriteTime;
+
+                    byte[] info = new UTF8Encoding(true).GetBytes(String.Format("{0,6:##0.000} {1}", delta.TotalSeconds, value));
+                    logFS.Write(info, 0, info.Length);
+                    lastWriteTime = currentTime;
+                }
+            }
+        }
+    }
+
+}
