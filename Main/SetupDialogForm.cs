78287e3c65dfabafc808d074fb4dc102170b76b2
Bret McKee <bretm@boneheads.us>
2010-10-30 22:21:04 -0600

    Added tooltips to the setup dialog

--- Main/SetupDialogForm.cs
+++ Main/SetupDialogForm.cs
@@ -42,39 +42,5 @@ namespace ASCOM.SXMain
                 MessageBox.Show(other.Message);
             }
         }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void EnableLoggingCheckBox_CheckedChanged(object sender, EventArgs e)
-        {
-        }
-
-        private void EnableUntestedCheckBox_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
-        {
-
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label2_Click_1(object sender, EventArgs e)
-        {
-
-        }
-
-        private void checkBox1_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
     }
 }
--- Main/SetupDialogForm.designer.cs
+++ Main/SetupDialogForm.designer.cs
@@ -28,6 +28,7 @@ namespace ASCOM.SXMain
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.cmdOK = new System.Windows.Forms.Button();
             this.cmdCancel = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
@@ -36,6 +37,7 @@ namespace ASCOM.SXMain
             this.EnableUntestedCheckBox = new System.Windows.Forms.CheckBox();
             this.Version = new System.Windows.Forms.Label();
             this.secondsAreMiliseconds = new System.Windows.Forms.CheckBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.picASCOM)).BeginInit();
             this.SuspendLayout();
             // 
@@ -47,6 +49,7 @@ namespace ASCOM.SXMain
             this.cmdOK.Size = new System.Drawing.Size(59, 24);
             this.cmdOK.TabIndex = 0;
             this.cmdOK.Text = "OK";
+            this.toolTip1.SetToolTip(this.cmdOK, "Close this dialog saving changes.");
             this.cmdOK.UseVisualStyleBackColor = true;
             this.cmdOK.Click += new System.EventHandler(this.cmdOK_Click);
             // 
@@ -58,18 +61,19 @@ namespace ASCOM.SXMain
             this.cmdCancel.Size = new System.Drawing.Size(59, 25);
             this.cmdCancel.TabIndex = 1;
             this.cmdCancel.Text = "Cancel";
+            this.toolTip1.SetToolTip(this.cmdCancel, "Close this dialog with no changes.");
             this.cmdCancel.UseVisualStyleBackColor = true;
             this.cmdCancel.Click += new System.EventHandler(this.cmdCancel_Click);
             // 
             // label1
             // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(361, 59);
+            this.label1.Size = new System.Drawing.Size(286, 76);
             this.label1.TabIndex = 2;
             this.label1.Text = "ASCOM Driver for SX/SXV/SXVF/SXVR Cameras\r\n\r\nCopyright (C) 2010 Dad Dog Developme" +
                 "nt Ltd.\r\nAll Rights Reserved";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // picASCOM
             // 
@@ -81,51 +85,53 @@ namespace ASCOM.SXMain
             this.picASCOM.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.picASCOM.TabIndex = 3;
             this.picASCOM.TabStop = false;
+            this.toolTip1.SetToolTip(this.picASCOM, "Click to visit the ASCOM web site.");
             this.picASCOM.DoubleClick += new System.EventHandler(this.BrowseToAscom);
             this.picASCOM.Click += new System.EventHandler(this.BrowseToAscom);
             // 
             // EnableLoggingCheckBox
             // 
             this.EnableLoggingCheckBox.AutoSize = true;
-            this.EnableLoggingCheckBox.Location = new System.Drawing.Point(15, 153);
+            this.EnableLoggingCheckBox.Location = new System.Drawing.Point(15, 123);
             this.EnableLoggingCheckBox.Name = "EnableLoggingCheckBox";
             this.EnableLoggingCheckBox.Size = new System.Drawing.Size(100, 17);
             this.EnableLoggingCheckBox.TabIndex = 4;
             this.EnableLoggingCheckBox.Text = "Enable Logging";
+            this.toolTip1.SetToolTip(this.EnableLoggingCheckBox, "Enable the creation of a debug log file.");
             this.EnableLoggingCheckBox.UseVisualStyleBackColor = true;
-            this.EnableLoggingCheckBox.CheckedChanged += new System.EventHandler(this.EnableLoggingCheckBox_CheckedChanged);
             // 
             // EnableUntestedCheckBox
             // 
             this.EnableUntestedCheckBox.AutoSize = true;
-            this.EnableUntestedCheckBox.Location = new System.Drawing.Point(15, 123);
+            this.EnableUntestedCheckBox.Location = new System.Drawing.Point(15, 155);
             this.EnableUntestedCheckBox.Name = "EnableUntestedCheckBox";
             this.EnableUntestedCheckBox.Size = new System.Drawing.Size(149, 17);
             this.EnableUntestedCheckBox.TabIndex = 5;
             this.EnableUntestedCheckBox.Text = "Enable Untested Cameras";
+            this.toolTip1.SetToolTip(this.EnableUntestedCheckBox, "Attempt to control untested cameras.");
             this.EnableUntestedCheckBox.UseVisualStyleBackColor = true;
-            this.EnableUntestedCheckBox.CheckedChanged += new System.EventHandler(this.EnableUntestedCheckBox_CheckedChanged);
             // 
             // Version
             // 
             this.Version.AutoSize = true;
+            this.Version.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Version.Location = new System.Drawing.Point(15, 88);
             this.Version.Name = "Version";
-            this.Version.Size = new System.Drawing.Size(72, 13);
+            this.Version.Size = new System.Drawing.Size(80, 15);
             this.Version.TabIndex = 6;
             this.Version.Text = "Version: a.b.c";
-            this.Version.Click += new System.EventHandler(this.label2_Click_1);
             // 
             // secondsAreMiliseconds
             // 
             this.secondsAreMiliseconds.AutoSize = true;
-            this.secondsAreMiliseconds.Location = new System.Drawing.Point(15, 187);
+            this.secondsAreMiliseconds.Location = new System.Drawing.Point(15, 188);
             this.secondsAreMiliseconds.Name = "secondsAreMiliseconds";
             this.secondsAreMiliseconds.Size = new System.Drawing.Size(147, 17);
             this.secondsAreMiliseconds.TabIndex = 7;
             this.secondsAreMiliseconds.Text = "Seconds Are Milliseconds";
+            this.toolTip1.SetToolTip(this.secondsAreMiliseconds, "Divide the requested exposure by 1000.  Useful if you need faster exposures than " +
+                    "your caputure software allows for.");
             this.secondsAreMiliseconds.UseVisualStyleBackColor = true;
-            this.secondsAreMiliseconds.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
             // SetupDialogForm
             // 
@@ -163,5 +169,6 @@ namespace ASCOM.SXMain
         public System.Windows.Forms.CheckBox EnableUntestedCheckBox;
         public System.Windows.Forms.Label Version;
         public System.Windows.Forms.CheckBox secondsAreMiliseconds;
+        private System.Windows.Forms.ToolTip toolTip1;
     }
 }
