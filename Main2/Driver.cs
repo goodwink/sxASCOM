083f88bf6c894fc97fa427329a18d56f7f446497
Bret McKee <bretm@boneheads.us>
2010-12-09 21:20:16 -0700

    Adding the files for the second main camera
    I should have added them a few commits ago, but I forgot

---
+++ Main2/Driver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using System.Runtime.InteropServices;
+
+using ASCOM;
+using ASCOM.Interface;
+
+using Logging;
+
+namespace ASCOM.SXMain2
+{
+    //
+    // Your driver's ID is ASCOM.SXCamera.Camera
+    //
+    // The Guid attribute sets the CLSID for ASCOM.SXCamera.Camera
+    // The ClassInterface/None addribute prevents an empty interface called
+    // _Camera from being created and used as the [default] interface
+    //
+    [Guid("23B414C0-6CB0-4f87-B0F7-00B2F8793D9C")]
+    [ClassInterface(ClassInterfaceType.None)]
+    public class Camera : ASCOM.SXMain.Camera
+    {
+        private const UInt16 DEFAULT_CAMERA_ID = 0;
+        private const UInt16 DEFAULT_CONTROLLER_NUMBER = 1;
+
+        public Camera() :
+            base(DEFAULT_CAMERA_ID, DEFAULT_CONTROLLER_NUMBER)
+        {
+            Log.Write(String.Format("Camera() called for main2\n"));
+        }
+    }
+}
