5b0f6bf9f512fc707a079a108ddc664ef7993fe4
Bret McKee <bretm@boneheads.us>
2011-01-30 20:11:55 -0700

    Adding absolute time to log messages

--- SXCamera/logging.cs
+++ SXCamera/logging.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using System.Text;
 using System.Collections.Generic;
+using System.Windows.Forms;
 
 namespace Logging
 {
@@ -10,13 +11,13 @@ namespace Logging
         private static string m_logPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) + "\\" + "ascom-sx-camera-log.txt";
         private static FileStream m_logFS = null;
         private static DateTime m_lastWriteTime;
+        private static DateTime m_startTime;
+        private static bool m_enabled;
 
         static Log()
         {
             try
             {
-                m_logFS = new FileStream(m_logPath, FileMode.Create, FileAccess.Write, FileShare.Read, 1);
-                m_lastWriteTime = DateTime.Now;
 #if DEBUG
                 enabled = true;
 #else
@@ -30,20 +31,52 @@ namespace Logging
 
         public static bool enabled
         {
-            get;
-            set;
+            get { return m_enabled; }
+            set
+            {
+                try
+                {
+                    if (value != m_enabled)
+                    {
+                        if (value)
+                        {
+                            m_logFS = new FileStream(m_logPath, FileMode.Create, FileAccess.Write, FileShare.Read, 1);
+                            m_lastWriteTime = DateTime.Now;
+                            m_startTime = m_lastWriteTime;
+                        }
+                        else
+                        {
+                            if (m_logFS != null)
+                            {
+                                m_logFS.Close();
+                                m_logFS = null;
+                            }
+                        }
+                        m_enabled = value;
+                    }
+                }
+                catch (System.Exception ex)
+                {
+                    m_enabled = false;
+#if DEBUG
+                    MessageBox.Show(String.Format("logging: enabled set caught an exception: {0}", ex));
+#endif
+                }
+            }
         }
 
-        public static void Write(string value)
+        public static void Write(string logMsg)
         {
-            if (m_logFS != null)
+            if (enabled)
             {
                 lock (m_logPath)
                 {
                     DateTime currentTime = DateTime.Now;
+                    TimeSpan elapsed = currentTime - m_startTime;
                     TimeSpan delta = currentTime - m_lastWriteTime;
 
-                    byte[] info = new UTF8Encoding(true).GetBytes(String.Format("{0,6:##0.000} {1}", delta.TotalSeconds, value));
+                    string message = String.Format("{0,9:####0.000} {1,7:##0.000} {2}", elapsed.TotalSeconds, delta.TotalSeconds, logMsg);
+                    byte[] info = new UTF8Encoding(true).GetBytes(message);
                     m_logFS.Write(info, 0, info.Length);
                     m_lastWriteTime = currentTime;
                 }
