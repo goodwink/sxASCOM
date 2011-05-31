9dbd2eccf57e6d119eb1c0f6dfbf198fe95add88
Bret McKee <bretm@boneheads.us>
2011-05-30 22:01:33 -0600

    Changes for version 1.5.3

--- SXCamera/sxGeneric/Driver.cs
+++ SXCamera/sxGeneric/Driver.cs
@@ -602,10 +602,10 @@ namespace ASCOM.SXGeneric
                 {
                     if (value)
                     {
-#if EXPIRE
-                        if (DateTime.Now.CompareTo(new DateTime(2011,6,1)) > 0)
+#if DEBUG
+                        if (DateTime.Now.CompareTo(new DateTime(2011,7,15)) > 0)
                         {
-                            MessageBox.Show("This non-production release has expired.  Please update your bits", "Expired");
+                            MessageBox.Show("This debug release has expired.  Please update your bits", "Expired");
                             throw new ASCOM.PropertyNotImplementedException(SetError("connected: non-production release expired"), true);
                         }
 #endif
