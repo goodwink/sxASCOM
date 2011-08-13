4057942e408eb208dc237c8f281260a541c9ee36
Bret McKee <bretm@boneheads.us>
2011-08-13 15:18:13 -0600

    Changes 2.2.0.3

--- SXAscomInstaller.iss
+++ SXAscomInstaller.iss
@@ -4,7 +4,7 @@
 ;
 
 #define BUILD_TYPE "Debug"
-#define APP_VERSION "2.2.0.2"
+#define APP_VERSION "2.2.0.3"
 #define ASCOM_VERSION_REQUIRED  "5.5"
 #define DRIVER_EXE_NAME "ASCOM.SXCamera.exe"
 
--- SXCamera/Properties/AssemblyInfo.cs
+++ SXCamera/Properties/AssemblyInfo.cs
@@ -46,5 +46,5 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 
-[assembly: AssemblyVersion("2.2.0.2")]
-[assembly: AssemblyFileVersion("2.2.0.2")]
+[assembly: AssemblyVersion("2.2.0.3")]
+[assembly: AssemblyFileVersion("2.2.0.3")]
