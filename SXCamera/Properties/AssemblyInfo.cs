a1dde65ba080a98f4bcbb2464ea3b520c295bd16
Bret McKee <bretm@boneheads.us>
2011-09-14 20:10:54 -0600

    Changes required for V2.2.0.4

--- SXCamera/Properties/AssemblyInfo.cs
+++ SXCamera/Properties/AssemblyInfo.cs
@@ -46,5 +46,5 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 
-[assembly: AssemblyVersion("2.2.0.3")]
-[assembly: AssemblyFileVersion("2.2.0.3")]
+[assembly: AssemblyVersion("2.2.0.4")]
+[assembly: AssemblyFileVersion("2.2.0.4")]
