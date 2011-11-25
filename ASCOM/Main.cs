f0359e34ba4585ad013e65a497cb71190d91a1cb
Bret McKee <bretm@boneheads.us>
2011-11-24 19:32:03 -0700

    More cleanup
    Rolled all the main camera files into Main.cs and all the
    guide camera files into Guide.cs

--- ASCOM/Main.cs
+++ ASCOM/Main.cs
@@ -370,3 +370,122 @@ namespace ASCOM.SXMain0
     }
 }
 
+namespace ASCOM.SXMain1
+{
+    //
+    // Your driver's ID is ASCOM.SXCamera.Camera
+    //
+    // The Guid attribute sets the CLSID for ASCOM.SXCamera.Camera
+    // The ClassInterface/None addribute prevents an empty interface called
+    // _Camera from being created and used as the [default] interface
+    //
+    [Guid("c150cbaa-429d-4bad-84ff-27077b4156a1")]
+    [ClassInterface(ClassInterfaceType.None)]
+    public class Camera : ASCOM.SXMain0.Camera
+    {
+        private const UInt16 DEFAULT_CONTROLLER_NUMBER = 1;
+        private const UInt16 DEFAULT_CAMERA_ID = 0;
+
+        public Camera() :
+            base(DEFAULT_CONTROLLER_NUMBER, DEFAULT_CAMERA_ID)
+        {
+            Log.Write(String.Format("Main1::Camera() executing\n"));
+        }
+    }
+}
+
+namespace ASCOM.SXMain2
+{
+    //
+    // Your driver's ID is ASCOM.SXCamera.Camera
+    //
+    // The Guid attribute sets the CLSID for ASCOM.SXCamera.Camera
+    // The ClassInterface/None addribute prevents an empty interface called
+    // _Camera from being created and used as the [default] interface
+    //
+    [Guid("c150cbaa-429d-4bad-84ff-27077b4156a2")]
+    [ClassInterface(ClassInterfaceType.None)]
+    public class Camera : ASCOM.SXMain0.Camera
+    {
+        private const UInt16 DEFAULT_CONTROLLER_NUMBER = 2;
+        private const UInt16 DEFAULT_CAMERA_ID = 0;
+
+        public Camera() :
+            base(DEFAULT_CONTROLLER_NUMBER, DEFAULT_CAMERA_ID)
+        {
+            Log.Write(String.Format("Main2::Camera() executing\n"));
+        }
+    }
+}
+
+namespace ASCOM.SXMain3
+{
+    //
+    // Your driver's ID is ASCOM.SXCamera.Camera
+    //
+    // The Guid attribute sets the CLSID for ASCOM.SXCamera.Camera
+    // The ClassInterface/None addribute prevents an empty interface called
+    // _Camera from being created and used as the [default] interface
+    //
+    [Guid("c150cbaa-429d-4bad-84ff-27077b4156a3")]
+    [ClassInterface(ClassInterfaceType.None)]
+    public class Camera : ASCOM.SXMain0.Camera
+    {
+        private const UInt16 DEFAULT_CONTROLLER_NUMBER = 3;
+        private const UInt16 DEFAULT_CAMERA_ID = 0;
+
+        public Camera() :
+            base(DEFAULT_CONTROLLER_NUMBER, DEFAULT_CAMERA_ID)
+        {
+            Log.Write(String.Format("Main3::Camera() executing\n"));
+        }
+    }
+}
+
+namespace ASCOM.SXMain4
+{
+    //
+    // Your driver's ID is ASCOM.SXCamera.Camera
+    //
+    // The Guid attribute sets the CLSID for ASCOM.SXCamera.Camera
+    // The ClassInterface/None addribute prevents an empty interface called
+    // _Camera from being created and used as the [default] interface
+    //
+    [Guid("c150cbaa-429d-4bad-84ff-27077b4156a4")]
+    [ClassInterface(ClassInterfaceType.None)]
+    public class Camera : ASCOM.SXMain0.Camera
+    {
+        private const UInt16 DEFAULT_CONTROLLER_NUMBER = 4;
+        private const UInt16 DEFAULT_CAMERA_ID = 0;
+            
+        public Camera() :
+            base(DEFAULT_CONTROLLER_NUMBER, DEFAULT_CAMERA_ID)
+        {
+            Log.Write(String.Format("Main4::Camera() executing\n"));
+        }
+    }
+}
+
+namespace ASCOM.SXMain5
+{
+    //
+    // Your driver's ID is ASCOM.SXCamera.Camera
+    //
+    // The Guid attribute sets the CLSID for ASCOM.SXCamera.Camera
+    // The ClassInterface/None addribute prevents an empty interface called
+    // _Camera from being created and used as the [default] interface
+    //
+    [Guid("c150cbaa-429d-4bad-84ff-27077b4156a5")]
+    [ClassInterface(ClassInterfaceType.None)]
+    public class Camera : ASCOM.SXMain0.Camera
+    {
+        private const UInt16 DEFAULT_CONTROLLER_NUMBER = 5;
+        private const UInt16 DEFAULT_CAMERA_ID = 0;
+
+        public Camera() :
+            base(DEFAULT_CONTROLLER_NUMBER, DEFAULT_CAMERA_ID)
+        {
+            Log.Write(String.Format("Main5::Camera() executing\n"));
+        }
+    }
+}
