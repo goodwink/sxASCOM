86a257cbff6fede7680846373d78d6235a018b8e
Bret McKee <bretm@boneheads.us>
2012-08-09 22:33:44 -0600

    Changes for 2.2.12222.2231

--- SXCamera/Properties/AssemblyInfo.cs
+++ SXCamera/Properties/AssemblyInfo.cs
@@ -24,5 +24,5 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 
-[assembly: AssemblyVersion("2.2.12221.1231")]
-[assembly: AssemblyFileVersion("2.2.12221.1231")]
+[assembly: AssemblyVersion("2.2.12222.2231")]
+[assembly: AssemblyFileVersion("2.2.12222.2231")]
