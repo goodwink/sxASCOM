4c3a9fb736fe419e5899cdb35dca903e3ac260cd
Bret McKee <bretm@boneheads.us>
2012-08-21 23:18:20 -0600

    Changes required for v2.2.12234.2312

--- SXCamera/Properties/AssemblyInfo.cs
+++ SXCamera/Properties/AssemblyInfo.cs
@@ -24,5 +24,5 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 
-[assembly: AssemblyVersion("2.2.12232.2005")]
-[assembly: AssemblyFileVersion("2.2.12232.2005")]
+[assembly: AssemblyVersion("2.2.12234.2312")]
+[assembly: AssemblyFileVersion("2.2.12234.2312")]
