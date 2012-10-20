a7b148d400ecd15c092b05f705ffd4f2c656cb14
Bret McKee <bretm@boneheads.us>
2012-10-19 21:39:39 -0600

    Changes for v2.2.12293.2135

--- FitsDataExtractor/Properties/AssemblyInfo.cs
+++ FitsDataExtractor/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 
-[assembly: AssemblyVersion("2.2.12251.1808")]
-[assembly: AssemblyFileVersion("2.2.12251.1808")]
+[assembly: AssemblyVersion("2.2.12293.2135")]
+[assembly: AssemblyFileVersion("2.2.12293.2135")]
--- SXCamera/Properties/AssemblyInfo.cs
+++ SXCamera/Properties/AssemblyInfo.cs
@@ -24,5 +24,5 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 
-[assembly: AssemblyVersion("2.2.12251.1808")]
-[assembly: AssemblyFileVersion("2.2.12251.1808")]
+[assembly: AssemblyVersion("2.2.12293.2135")]
+[assembly: AssemblyFileVersion("2.2.12293.2135")]
